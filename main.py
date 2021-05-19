73f1492055aa6710d40d117bc14b14b93bfd5b12
sion9840 <sioning611@gmail.com>
2021-05-19 21:43:00 +0900

    commit

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from sklearn.linear_model import LinearRegression
 import streamlit as st
 import pandas as pd
 
+st.title("Analysis Sat")
+st.header("수능을 분석한 결과를 보여주는 사이트")
+st.info("분석 수능 과목 : 수학")
+
+st.markdown('''---''')
+
 problem_len = 30
 choice_len = 6
 exam_answer = {
@@ -19,12 +25,6 @@ exam_len = exam_answer_keys.shape[0]
 problem_answer = np.array([np.array([exam_answer_values[j, i] for j in range(exam_len)]) for i in range(problem_len)])
 problem_answer_per = np.array([np.bincount(i).argmax() for i in problem_answer])
 
-st.title("Analysis Sat")
-st.header("수능을 분석한 결과를 보여주는 사이트")
-st.info("분석 수능 과목 : 수학")
-
-st.markdown('''---''')
-
 st.write("년도별 수능 답 번호")
 df_ea = pd.DataFrame(exam_answer_values, index=exam_answer_keys, columns=np.arange(1, problem_len+1))
 st.dataframe(df_ea)
@@ -38,8 +38,21 @@ for i in range(problem_len):
     line_fitter = LinearRegression()
     line_fitter.fit(np.arange(1, exam_len+1).reshape(-1, 1), problem_answer[i])
 
+    line = np.arange(exam_len*2)
+    line1 = problem_answer[i]
+    line2 = np.array([line_fitter.predict([[i]]) for i in range(exam_len)])
+
+    a, b = 0, 0
+    for j in range(exam_len):
+        if j % 2 == 0:
+            line[j] = line1[a]
+            a += 1
+        else:
+            line[j] = line2[b]
+            b += 1
+
     chart_data = pd.DataFrame(
-        [problem_answer[i],[line_fitter.predict([[0]]), line_fitter.predict([[exam_len+1]])]],
+        line,
         columns=['problem answer', 'predict line']
         )
    
